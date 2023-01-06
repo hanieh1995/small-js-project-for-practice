24b1d71878850486fa42d3e02d0338bb9d4fd13b
Hanieh1995 <hony1374@gmail.com>
2023-01-06 12:44:56 +0330

    add localstorage

--- todo-list/script.js
+++ todo-list/script.js
@@ -4,19 +4,36 @@ let list = document.querySelector(".list");
 let clearBtn = document.querySelector(".clearBtn");
 let alarm = document.querySelector(".alarm");
 
-list.addEventListener("click" , (e)=>{
-  if (e.target.className == "fa fa-times-circle") {
+let itemsList = JSON.parse(localStorage.getItem("itemsList")) || [];
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-times-circle")) {
+    let index = Array.from(list.children).findIndex(
+      (item) => item == e.target.parentNode
+    );
+    itemsList.splice(index, 1);
     e.target.parentNode.remove();
   }
-  if(e.target.className == "fa fa-check-circle"){
+  if (e.target.classList.contains("fa-check-circle")) {
     e.target.parentNode.firstChild.classList.toggle("done-item");
     e.target.classList.toggle("done");
+    if (e.target.classList.contains("done")) {
+      let index = Array.from(list.children).findIndex(
+        (item) => item == e.target.parentNode
+      );
+      itemsList[index].condition = true;
+    }
   }
-  if(e.target.className == "fa fa-edit"){
+  if (e.target.classList.contains("fa-edit")) {
     input.value = e.target.parentNode.firstChild.innerText;
-       e.target.parentNode.remove();
+    e.target.parentNode.remove();
+    let index = Array.from(list.children).findIndex(
+      (item) => item == e.target.parentNode
+    );
+    itemsList[index].splice(index, 1);
   }
-})
+  localStorage.setItem("itemsList", JSON.stringify(itemsList));
+});
 
 addBtn.addEventListener("click", (e) => {
   if (!input.value) {
@@ -25,24 +42,42 @@ addBtn.addEventListener("click", (e) => {
     }, 3000);
     alarm.style.display = "block";
   } else {
-    list.appendChild(createListItem(input.value));
+    let listItemsInfo = new ListItemsInfo(input.value, false);
+    itemsList.push(listItemsInfo);
+    localStorage.setItem("itemsList", JSON.stringify(itemsList));
+    list.innerHTML = "";
+    itemsList.forEach((element) => {
+      list.appendChild(createListItem(element.input, element.condition));
+    });
     input.value = "";
   }
 });
 
-function createListItem(inputValue) {
+if (itemsList.length != 0) {
+  list.innerHTML = "";
+  itemsList.forEach((element) => {
+    list.appendChild(createListItem(element.input, element.condition));
+  });
+}
+
+function createListItem(inputValue, condition) {
   let li = document.createElement("li");
   let title = document.createElement("span");
   let done = document.createElement("i");
   let edit = document.createElement("i");
   let remove = document.createElement("i");
 
-  title.className = "title";
+  if (condition) {
+    done.classList.add("done");
+    title.classList.add("done-item");
+  }
+
+  title.classList.add("title");
   title.innerText = inputValue;
 
-  done.className = "fa fa-check-circle";
-  edit.className = "fa fa-edit";
-  remove.className = "fa fa-times-circle";
+  done.classList.add("fa-check-circle" , "fa");
+  edit.classList.add("fa-edit" , "fa");
+  remove.classList.add("fa-times-circle" , "fa");
 
   li.appendChild(title);
   li.appendChild(done);
@@ -56,4 +91,11 @@ clearBtn.addEventListener("click", () => {
   Array.from(list.children).forEach((element) => {
     element.remove();
   });
+  itemsList = [];
+  localStorage.clear();
 });
+
+function ListItemsInfo(input, condition) {
+  this.input = input;
+  this.condition = condition;
+}
